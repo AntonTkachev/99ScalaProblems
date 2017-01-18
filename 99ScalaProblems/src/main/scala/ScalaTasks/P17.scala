2c03896c6c6a36f8581c9f0d4e1aa8fbdc8c9a6b
Anton Tkachev <dgeneros@mail.ru>
2017-01-19 00:07:43 +0300

    Added 17's problem

---
+++ 99ScalaProblems/src/main/scala/ScalaTasks/P17.scala
@@ -0,0 +1,32 @@
+package ScalaTasks
+
+import scala.collection.mutable.ListBuffer
+
+object P17 extends App {
+
+  /** P17 (*) Split a list into two parts.
+    * The length of the first part is given. Use a Tuple for your result.
+    * Example:
+    * *
+    * scala> split(3, List('a, 'b, 'c, 'd, 'e, 'f, 'g, 'h, 'i, 'j, 'k))
+    * res0: (List[Symbol], List[Symbol]) = (List('a, 'b, 'c),List('d, 'e, 'f, 'g, 'h, 'i, 'j, 'k))
+    * **/
+
+  def split(num: Int, list: List[Symbol]): Unit = {
+    var firstList = new ListBuffer[Symbol]()
+    var secondList = new ListBuffer[Symbol]()
+    var length = 1
+    list.foreach({ l => l
+      if (length <= num) {
+        firstList += l
+      }
+      else {
+        secondList += l
+      }
+      length += 1
+    })
+    println(firstList.toList, secondList.toList)
+  }
+
+  split(3, List('a, 'b, 'c, 'd, 'e, 'f, 'g, 'h, 'i, 'j, 'k))
+}
