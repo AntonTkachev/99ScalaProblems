dd2dfdc004e9b9e7f7bc60043e2ae40d496bcfa7
AntonTkachev <dgeneros@mail.ru>
2017-02-20 18:26:04 +0300

    Added method

--- 99ScalaProblems/src/main/scala/TreeParser/StrParse.scala
+++ 99ScalaProblems/src/main/scala/TreeParser/StrParse.scala
@@ -1,12 +1,12 @@
 import scala.collection.mutable.ListBuffer
-import util.control.Breaks._
+import scala.util.control.Breaks._
 
 object StrParse extends App {
 
   val example =
     """TAG
-      |	first
-      |	first1
+      |	first =52
+      |	first =11
       |	first2
       |
       |TAG1
@@ -38,7 +38,8 @@ object StrParse extends App {
         if (message.nonEmpty) {
           val headMsg = message.head
           val numTabulation = headMsg.split("|").count(_ == "\t")
-          if (headMsg.replaceAll("\\W+", "").equals(findTag) && numTabulation < 2) { // TODO подумать над цифрой 2
+          if (headMsg.replaceAll("\\W+", "").equals(findTag) && numTabulation < 2) {
+            // TODO подумать над цифрой 2
             endList += headMsg
             childrenFindTag(message.tail, numTabulation)
           }
@@ -75,9 +76,35 @@ object StrParse extends App {
       rootTag
     }
 
+    def getTag(findTag: String): Boolean = {
+      var result = false
+      msg.split("\n").foreach({ line =>
+        val xx = line.split("=") //TODO rename
+        if (xx.head.replaceAll("\\W+", "") == findTag) {
+          result = true
+        }
+      })
+      result
+    }
+
+    def getValue(findTag: String) = {
+      msg.split("\n").flatMap({ line =>
+        val pv = line.split("=")
+        if (pv.head.replaceAll("\\W+", "") == findTag && pv.size > 1) {
+          Option(pv(1).trim).find(_.nonEmpty)
+        }
+        else None
+      })
+    }
+
   }
 
-  (example \ "TAG2").foreach(println)
+  //    println(example.getTag("first"))
+
+  example.getValue("first").foreach(println)
+
+  //  (example \ "TAG2").foreach(println)
 
   //  print(example.getRoot)
+
 }
