f8943e30d4688b1c0bb0c1b06df87ae572a84578
AntonTkachev <dgeneros@mail.ru>
2017-02-21 18:44:32 +0300

    Added model for parser

---
+++ 99ScalaProblems/src/main/scala/TreeParser/Node.scala
@@ -0,0 +1,42 @@
+package model
+
+class Node {
+
+  var rootTag: String = _
+
+  var tagValue: Map[String, String] = _
+
+  var rootChildren: List[String] = _
+
+
+  def getRoot = {
+    require(rootTag != null)
+    this.rootTag
+  }
+
+  def getTag(tag: String): Boolean = {
+    require(tagValue != null)
+    var result = false
+    tagValue.foreach({ x =>
+      if (x._1 == tag) {
+        result = true
+      }
+    })
+    result
+  }
+
+  def getValue(tag: String) = {
+    require(tagValue != null)
+    tagValue.flatMap({ x =>
+      if (x._1 == tag && x._2.nonEmpty) {
+        Option(x._2)
+      }
+      else None
+    })
+  }
+
+  def getChildren = {
+    require(rootChildren != null)
+    this.rootChildren
+  }
+}
