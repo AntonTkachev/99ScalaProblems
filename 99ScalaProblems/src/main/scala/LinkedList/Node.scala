6d175767ed36ace0a6a0b29ffb7e030d860f058a
Anton Tkachev <dgeneros@mail.ru>
2017-03-18 13:59:03 +0300

    Added method insert and fix dellNum

--- 99ScalaProblems/src/main/scala/LinkedList/Node.scala
+++ 99ScalaProblems/src/main/scala/LinkedList/Node.scala
@@ -76,22 +76,65 @@ class LinkedList {
     list.size -= 1
   }
 
-  /*TODO не хочу index передавать, подумать над настройкой _prev _next*/
-  def getElem(elem: Node, index: Long): Node = {
-
-    if (elem.value != index) {
-      getElem(elem._prev, index)
-    }
+  //TODO переписать
+  def xx(elem: Node, index: Long): Node = {
+    if (elem.value + 1 != index) xx(elem._next, index)
     else elem
   }
 
+  def netNthq(list: DblLinkedList, index: Long) : Node = {
+    var tmp = new Node
+    var i = 0
+
+    if (index > list.size / 2) {
+      i = list.size - 1
+      tmp = list.tail
+      while (i < index) {
+        tmp = tmp._prev
+        i -= 1
+      }
+    }
+    else {
+      tmp = list.head
+      while (i < index) {
+        tmp = tmp._next
+        i += 1
+      }
+    }
+    tmp
+  }
+
   def dellNum(list: DblLinkedList, index: Long) = {
-    require(list.size > index, "Индекс больше колличества членов")
+    require(list.size > index || index < 0, "Неверное значение индекса")
+
+    val requiredElem = netNthq(list, index)
+    if (requiredElem._next != null) {
+      requiredElem._next._prev = requiredElem._prev
+    }
+    else {
+      requiredElem._next = list.head
+    }
+    if (requiredElem._prev != null) {
+      requiredElem._prev._next = requiredElem._next
+    }
+    else {
+      requiredElem._prev = list.tail
+    }
+
     list.size -= 1
-    val requiredElem = getElem(list.tail, index)
-    requiredElem._next._prev = requiredElem._prev
-    requiredElem._prev._next = requiredElem._next
-    requiredElem
+  }
+
+  def inset(list: DblLinkedList, index: Long, value: Int) = {
+    if (index < 0 || index > list.size + 1) {
+      throw new IllegalArgumentException("Неверное значение индекса")
+    }
+    val requiredElem = xx(list.head, index)
+    val tmp = new Node
+    tmp.value = value
+    tmp._prev = requiredElem
+    tmp._next = requiredElem._next
+    requiredElem._next = tmp
+    list.size += 1
   }
 }
 
@@ -102,8 +145,12 @@ object Test extends App {
 
   linkList.addFront(list, 2)
   linkList.addFront(list, 1)
+  linkList.addFront(list, 0)
   linkList.addBack(list, 3)
-  linkList.dellNum(list, 2)
+  linkList.addBack(list, 4)
+  linkList.inset(list, index = 4, value = 5)
+  linkList.netNthq(list, 5)
+  linkList.dellNum(list, 5)
   linkList.dellFront(list)
   println(list)
 }
