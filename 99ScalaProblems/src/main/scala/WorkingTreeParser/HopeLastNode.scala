3f4d59f5d8ce386c33fd37bba1e1f8d832e229e0
AntonTkachev <dgeneros@mail.ru>
2017-04-03 12:10:13 +0300

    Fix parse method and added spec's

--- 99ScalaProblems/src/main/scala/WorkingTreeParser/HopeLastNode.scala
+++ 99ScalaProblems/src/main/scala/WorkingTreeParser/HopeLastNode.scala
@@ -21,34 +21,51 @@ class HopeLastNode extends StrictLogging {
     listRootTags.length match {
       case 1 =>
         val clearArray = myStructure.filter(elem => elem != "\r" && elem != " ")
-        f(clearArray)
-      case _ => logger.error("Root tag should be one")
+        iterator(clearArray)
+      case _ => throw new Throwable("Root tag should be one")
     }
 
-    def f(str: Array[String]): Unit = {
+    def iterator(str: Array[String]): Unit = {
       if (str.head.split("|").head.matches("\\w+")) {
         rootTag = str.head
-        f(str.tail)
+        iterator(str.tail)
       }
       else if (numOfTab(str.head) == numOfTab(str(1))) {
-        listChildren += str.head
-        if (str.tail.length != 1) f(str.tail)
+        if (str.tail.length != 1) {
+          listChildren += str.head
+          iterator(str.tail)
+        }
         else {
-          listChildren += str(1)
-          parserList.addNext(rootTag, listChildren.toList)
+          if (str.head == rootTag) {
+            parserList.addNext(rootTag)
+            parserList.addNext(str(1))
+          }
+          else {
+            listChildren += str.head
+            listChildren += str(1)
+            parserList.addNext(rootTag, listChildren.toList)
+          }
         }
       }
       else if (numOfTab(str.head) < numOfTab(str(1))) {
-        parserList.addNext(rootTag, listChildren.toList)
+//        listChildren += str.head
+        if (rootTag.nonEmpty) parserList.addNext(rootTag, listChildren.toList)
         rootTag = str.head
         listChildren.clear()
-        if (str.tail.length != 1) f(str.tail)
+        if (str.tail.length != 1) iterator(str.tail)
         else {
           parserList.addNext(rootTag = str.head)
           parserList.addNext(rootTag = str(1))
         }
+
       }
-      else str
+      else if (numOfTab(str.head) > numOfTab(str(1))) {
+        listChildren += str.head
+        parserList.addNext(rootTag, listChildren.toList)
+        listChildren.clear()
+        rootTag = str(1)
+        iterator(str.tail)
+      }
     }
 
     parserList
--- 99ScalaProblems/src/test/scala/WorkingTreeParser/HopeLastNodeSpec.scala
+++ 99ScalaProblems/src/test/scala/WorkingTreeParser/HopeLastNodeSpec.scala
@@ -6,7 +6,7 @@ class HopeLastNodeSpec extends FlatSpec with Matchers {
   val exampleWithManyRootTag =
     """TAG
       |	first =52
-      |	first =11
+      |	first1 =11
       |	first2
       |
       |TAG1
@@ -19,7 +19,7 @@ class HopeLastNodeSpec extends FlatSpec with Matchers {
       |
       |			TAG3
       |				fourth =11
-      |				fourth =213
+      |				fourth1 =213
       |				fourth2
       |				fourth3
       |
@@ -31,7 +31,7 @@ class HopeLastNodeSpec extends FlatSpec with Matchers {
   val exampleWithOneRootTag =
     """TAG
       |	first =52
-      |	first =11
+      |	first1 =11
       |	first2
       |
       |	TAG2
@@ -40,9 +40,69 @@ class HopeLastNodeSpec extends FlatSpec with Matchers {
       |
       |		TAG4
       |			fourth =11
-      |			fourth =213
+      |			fourth1 =213
       |			fourth2
       |			fourth3
+      |
+      | """.stripMargin
+
+  val exampleStartWithNonRootTag =
+    """	TAG2
+      |		second =1
+      |		second1
+      |
+      |TAG
+      |	first =52
+      |	first1 =11
+      |	first2
+      |
+      |		TAG4
+      |			fourth =11
+      |			fourth1 =213
+      |			fourth2
+      |			fourth3
+      | """.stripMargin
+
+  val exampleEndTwoTag = //TODO incorrectly example???
+    """TAG
+      |	first =52
+      |	first1 =11
+      |	first2
+      |
+      |	TAG2
+      |		second =1
+      |		second1
+      |
+      |		TAG4
+      |			fourth =11
+      |			fourth1 =213
+      |			fourth2
+      |			fourth3
+      |
+      |		TAG4.1
+      |
+      |		TAG4.2
+      | """.stripMargin
+
+  val exampleLessEnd = //TODO spec with this
+    """TAG
+      |	first =52
+      |	first1 =11
+      |	first2
+      |
+      |	TAG2
+      |		second =1
+      |		second1
+      |
+      |		TAG4
+      |			fourth =11
+      |			fourth1 =213
+      |			fourth2
+      |			fourth3
+      |
+      |		TAG4.1
+      |			fifth
+      |		TAG4.2
       | """.stripMargin
 
   val newNode = new HopeLastNode()
@@ -51,17 +111,17 @@ class HopeLastNodeSpec extends FlatSpec with Matchers {
   def parser(str: String) = newNode.parser(str)
 
   it should "return ex" in {
-    parser(exampleWithManyRootTag) //TODO отловить ошибку?
+    a[Throwable] shouldBe thrownBy(parser(exampleWithManyRootTag))
   }
 
   it should "x3" in {
     val result = parser(exampleWithOneRootTag)
-    helper.printList(result) //TODO как нибудь засунуть в спеку
+    helper.listToString(result).replaceAll("\\s","") shouldEqual exampleWithOneRootTag.replaceAll("\\s","") //TODO dirty hack
   }
 
   it should "return children of rootTag" in {
     val result = parser(exampleWithOneRootTag)
-    helper.getChildren(result).toArray shouldEqual Array("first =52\r", "first =11\r", "first2\r")
+    helper.getChildren(result).toArray shouldEqual Array("first =52\r", "first1 =11\r", "first2\r")
   }
 
   it should "return children of tag TAG2" in {
@@ -72,5 +132,15 @@ class HopeLastNodeSpec extends FlatSpec with Matchers {
   it should "return root tag" in {
     val result = parser(exampleWithOneRootTag)
     helper.returnRootTag(result) shouldEqual "TAG\r"
+  }
+
+  it should "save structure????" in {
+    val result = parser(exampleStartWithNonRootTag)
+    result
+  }
+
+  it should "return two last root tag without children" in {
+    val result = parser(exampleEndTwoTag)
+    result
   }
 }
